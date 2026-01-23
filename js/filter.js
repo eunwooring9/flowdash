ec0d071c2c75a167b5b5b897b3f323dd9eb3309f
h7-r <dlaudwo2311@gmail.com>
2026-01-23 10:46:29 +0900

    feat : nickname jss, css 수정 및 작업

--- js/filter.js
+++ js/filter.js
@@ -3,20 +3,20 @@
 const dropdown = document.querySelectorAll(".filter-dropdown");
 
 dropdown.forEach((dropdown) => {
-   const btn = dropdown.querySelector(".filter-btn");
-   const mainText = dropdown.querySelector(".filter-btn span");
-   const items = dropdown.querySelectorAll(".dropdown-item");
+  const btn = dropdown.querySelector(".filter-btn");
+  const mainText = dropdown.querySelector(".filter-btn span");
+  const items = dropdown.querySelectorAll(".dropdown-item");
 
-   items.forEach((item) => {
-      item.addEventListener("click", () => {
-         mainText.textContent = item.textContent;
-         dropdown.classList.remove("active");
-      });
-   });
+  items.forEach((item) => {
+    item.addEventListener("click", () => {
+      mainText.textContent = item.textContent;
+      dropdown.classList.remove("active");
+    });
+  });
 
-   btn.addEventListener("click", () => {
-      dropdown.classList.toggle("active");
-   });
+  btn.addEventListener("click", () => {
+    dropdown.classList.toggle("active");
+  });
 });
 
 // 오름차순 / 내림차순
@@ -25,11 +25,11 @@ const sortText = document.querySelector("#sort-text");
 const priorityBtn = document.querySelector("#btn-priority");
 
 sortBtn.addEventListener("click", function () {
-   if (sortText.textContent === "오름차순") {
-      sortText.textContent = "내림차순";
-      priorityBtn.textContent = "내림차순";
-   } else {
-      sortText.textContent = "오름차순";
-      priorityBtn.textContent = "오름차순";
-   }
+  if (sortText.textContent === "오름차순") {
+    sortText.textContent = "내림차순";
+    priorityBtn.textContent = "내림차순";
+  } else {
+    sortText.textContent = "오름차순";
+    priorityBtn.textContent = "오름차순";
+  }
 });
